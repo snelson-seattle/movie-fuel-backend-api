27b8544e4317fdeaecadb31f3ddebb78f0ccc86a
Scott Nelson <snelson@instructors.2u.com>
2023-10-22 15:19:54 -0700

    fixed jwt middleware

--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const verifyJWT = (req, res, next) => {
   const authHeader = req.headers.authorization || req.headers.Authorization;
 
-  if (!authHeader?.startsWith("Bearer ")) {
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return res.status(401).json({ message: "Unauthorized" });
   }
 
